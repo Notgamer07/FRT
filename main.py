50dae2cb9519c7e0995fef1b114209c9b202c373
Notgamer07 <tyjagrath@gmail.com>
2025-06-06 20:35:14 +0530

    now blocks' speed increase on levels -3,6,9

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from front import falling_button, falling_animation, draw_defeat, draw_label, draw_background, draw_score, menu_button
 from datahandle import Question
-
+import random
 
 def collision(obj, player, button)->bool:
     if player.colliderect(button.rect):
@@ -31,8 +31,8 @@ speed = 400
 direction = pygame.math.Vector2(0,0)
 
 score = 0
-level = 1
-questionNumber = 1
+level = 2
+questionNumber = 2
 
 q = Question(level,questionNumber)
 buttons = []
@@ -45,7 +45,7 @@ current_screen = 'menu'
 blocksTime = 0
 while run:
     globalTime = pygame.time.get_ticks()
-    dt = clock.tick(60) / 1000 #delta time in seconds
+    dt = clock.tick(120) / 1000 #delta time in seconds
     screen.fill('black')
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -78,10 +78,25 @@ while run:
         continue
     
     '''GAME SCREEN'''
+
+    #Timer to summon Falling Block
     if (globalTime - blocksTime) >= cooldownTimer:
         blocksTime = pygame.time.get_ticks()
         buttons = falling_button(q, buttons)
-    
+        if cooldownTimer >= 1200:
+            cooldownTimer = 2000 - (level + questionNumber) * 100
+        cooldownTimer += random.randint(0,5)*50  #adding 0.0 to 0.5s randomness in summoning falling blocks
+
+    # Speed of Falling Blocks
+    if level < 3 :
+        speedOfBlock = 200
+    elif level < 6 and speedOfBlock != 240:
+        speedOfBlock = 240
+    elif level < 9 and level >= 6 and speedOfBlock != 290:
+        speedOfBlock = 290
+    elif level <=10 and level >= 9 and speedOfBlock != 350:
+        speedOfBlock = 350
+
     # Player Movement
     for button in buttons:
         isCorrect = collision(q,player,button)
@@ -91,7 +106,7 @@ while run:
                 questionNumber += 1
                 q = Question(level,questionNumber)
                 buttons.remove(button)
-            elif level <10:
+            elif level < 10:
                 questionNumber = 1
                 level += 1
                 questionNumber += 1
@@ -113,7 +128,7 @@ while run:
 
     # we draw  background->button ->label->player .. so that player is above button and button is behind label
     draw_background(screen)
-    falling_animation(screen,buttons,dt)
+    falling_animation(screen,buttons,dt, speedOfBlock)
     draw_label(screen,q.question)
     pygame.draw.rect(screen,playerColor,player,border_radius = 8)
     draw_score(screen,score)
